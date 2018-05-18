bf68df137e183473a87376516701f95b1c2bea08
Christoph Wiesner <christoph.wiesner@runtastic.com>
2018-05-18 11:56:19 +0200

    Initial commit

---
+++ testapp/src/main/java/com/example/collapsingtoolbar/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.collapsingtoolbar.testapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_back)
+    }
+}
